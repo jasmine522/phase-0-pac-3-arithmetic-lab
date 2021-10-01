9fd99bd6f39d194be98b04c48ed9628007c0beb9
Jasmine Duruji <jasminedrj@gmail.com>
2021-09-30 21:08:52 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+let number = 5;
+function add() {
+  return (number += 5);
+}
+function subtract() {
+  return (number -= 5);
+}
+function multiply() {
+  return (number *= 5);
+}
+function divide() {
+  return (number /= 5);
+}
+
+let a = 5;
+let b = 5;
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function increment(a) {
+  return a + 1;
+}
+function decrement(a) {
+  return a - 1;
+}
+
+function makeInt(n) {
+  return n;
+}
+function makeInt(n) {
+  return parseInt(n);
+}
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+  return parseFloat("2.222");
+}
